131976af4c3666a10b205cdb7ad6b8f25388e3d3
kdt4-mh <alsgur6811@gmail.com>
2024-05-01 09:08:23 +0900

    디자인패턴 싱글톤, 템플릿메서드 패턴

---
+++ src/useful/ch04/PlayerMainTest.java
@@ -0,0 +1,11 @@
+package useful.ch04;
+
+public class PlayerMainTest {
+
+	public static void main(String[] args) {
+		
+		Player player = Player.getInstance();
+		
+		System.out.println(player);
+	}
+}
